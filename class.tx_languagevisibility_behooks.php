3e97efa7f865418186fc4cda03ba6de76e4f9418
timo_aoemedia <timo_aoemedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-21 14:31:04 +0000

    * fixed detection of elements in hook which have empty visibility setting now and where not empty before * increased version number
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/languagevisibility/trunk@23557 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_languagevisibility_behooks.php
+++ class.tx_languagevisibility_behooks.php
@@ -122,7 +122,10 @@ class tx_languagevisibility_behooks {
 	 * @author Fabrizio Branca <fabrizio.branca@aoemedia.de>
 	 */
 	public function aoewspreview_createDiff(array $params) {
+
+
 		$element = $params['element'];
+
 		if (/* ($params['table'] == 'tt_content') && */ ($params['fieldName'] == 'tx_languagevisibility_visibility')) {
 			$diff = array();
 
@@ -132,24 +135,38 @@ class tx_languagevisibility_behooks {
 			$recordNew = is_array($recordNew) ? $recordNew : array();
 			$recordOld = is_array($recordOld) ? $recordOld : array();
 
-			foreach(array_keys(array_diff_assoc($recordNew, $recordOld)) as $key) {
-				if (empty($recordOld[$key]) && ($recordNew[$key] == '-')) {
-					// this is equal, too!
-					//we need to inform the user what happens because he doesn't understand
-					//what happend if the field tx_languagevisibility_visibility was configured as critical field
-					//and if was just initialized with default values.
-					$diff[] = sprintf('%s Visibility was initialized with the default value (%s)',
-						tx_mvc_common_typo3::getLanguageFlag($key, $params['newRecord']['pid']),
-						$recordNew[$key]
-					);
-				} else {
-
-					$diff[] = sprintf('%s Visibility changed from <span class="diff-r">%s</span> to <span class="diff-g">%s</span>',
-						tx_mvc_common_typo3::getLanguageFlag($key, $params['newRecord']['pid']),
-						$GLOBALS['LANG']->sL('LLL:EXT:languagevisibility/locallang_db.xml:tx_languagevisibility_visibility.I.'.$recordOld[$key]),
-						$GLOBALS['LANG']->sL('LLL:EXT:languagevisibility/locallang_db.xml:tx_languagevisibility_visibility.I.'.$recordNew[$key])
-					);
+			
+			$equalInBoth				 	= array_intersect_assoc($recordNew,$recordOld);			
+			$changedKeyOld 					= array_keys(array_diff_assoc($recordOld,$equalInBoth)); 
+			$changedKeyNew 					= array_keys(array_diff_assoc($recordNew,$equalInBoth)); 
+			$keyOfChangedLanguageSettings	= array_unique(array_merge($changedKeyNew,$changedKeyOld));			
+			
+			if(is_array($keyOfChangedLanguageSettings) && (count($keyOfChangedLanguageSettings) > 0)){
+				foreach($keyOfChangedLanguageSettings as $key) {
+					if (empty($recordOld[$key]) && ($recordNew[$key] == '-')) {
+						// this is equal, too!
+						//we need to inform the user what happens because he doesn't understand
+						//what happend if the field tx_languagevisibility_visibility was configured as critical field
+						//and if was just initialized with default values.
+						$diff[] = sprintf('%s Visibility was initialized with the default value (%s)',
+							tx_mvc_common_typo3::getLanguageFlag($key, $params['newRecord']['pid']),
+							$recordNew[$key]
+						);
+					} elseif(empty($recordNew[$key]) ){
+						//in this case an old visibility setting has been changed to an empty value,
+						//this can happen when a new workspace version is created
+						$diff[0] = 'Languagevisibility was changed from '.serialize($recordOld).' to '.serialize($recordNew);
+					}else {
+				
+						$diff[] = sprintf('%s Visibility changed from <span class="diff-r">%s</span> to <span class="diff-g">%s</span>',
+							tx_mvc_common_typo3::getLanguageFlag($key, $params['newRecord']['pid']),
+							$GLOBALS['LANG']->sL('LLL:EXT:languagevisibility/locallang_db.xml:tx_languagevisibility_visibility.I.'.$recordOld[$key]),
+							$GLOBALS['LANG']->sL('LLL:EXT:languagevisibility/locallang_db.xml:tx_languagevisibility_visibility.I.'.$recordNew[$key])
+						);
+					}
 				}
+			}else{
+				//in this case the structure of the languagevisibility was changed but no visibility setting 
 			}
 
 			if (count($diff) > 0) {
@@ -159,8 +176,11 @@ class tx_languagevisibility_behooks {
 				$element = false;
 			}
 		}
+
 		return $element;
 	}
 }
+
+	
 
 ?>
