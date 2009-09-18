01b4ba7d7824fa6b9e5873a70475d887bd7e44d0
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-18 16:06:50 +0000

    added some environment tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/languagevisibility/trunk@24642 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/tx_environment_testcase.php
@@ -0,0 +1,50 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 Tolleiv Nietsch <nietsch@aoemedia.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class tx_environment_testcase extends tx_phpunit_database_testcase {
+
+	/**
+	 * Just to have some confidence about the system settings ;)
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function pageOverlayFieldExists() {
+		$list = t3lib_div::trimExplode(",",$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields']);
+		$this->assertEquals(true,in_array('tx_languagevisibility_inheritanceflag_overlayed',$list),'tx_languagevisibility_inheritanceflag_overlayed missing in $GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'pageOverlayFields\']');
+
+	}
+	/**
+	 * Just to have some confidence about the system settings ;)
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function rootlineFieldExists() {
+		$list = t3lib_div::trimExplode(",",$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields']);
+		$this->assertEquals(true,in_array('tx_languagevisibility_inheritanceflag_original',$list),'tx_languagevisibility_inheritanceflag_original missing in ,$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'addRootLineFields\']');
+		$this->assertEquals(true,in_array('tx_languagevisibility_inheritanceflag_overlayed',$list),'tx_languagevisibility_inheritanceflag_overlayed missing in ,$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'addRootLineFields\']');
+	}
+}
+?>
