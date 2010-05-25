7427368ea2a651fb6369e52ed2da763897c369f6
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-25 13:19:45 +0000

    minor CGL changes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/languagevisibility/trunk@33446 735d13b6-9817-0410-8766-e36946ffe9aa

--- patch/lib/class.tx_languagevisibility_beUser.php
+++ patch/lib/class.tx_languagevisibility_beUser.php
@@ -1,46 +1,71 @@
 <?php
-class tx_languagevisibility_beUser{
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2009 AOE media <dev@aoemedia.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ *
+ * @author	Daniel Pötzinger
+ */
+class tx_languagevisibility_beUser {
 	private $be_user;
-	
-	public function __construct(){
+
+	public function __construct() {
 		global $BE_USER;
-		
-		$this->be_user = $BE_USER;	
+
+		$this->be_user = $BE_USER;
 	}
-	
+
 	/**
 	 * This Method determines if the option allow_movecutdelete_foroverlays has been
-	 * set. It 
+	 * set. It
 	 *
 	 * @return unknown
 	 */
-	function allowCutCopyMoveDelete(){
-		$res = false;	
-		if(is_array($this->be_user->userGroups)){
-			foreach($this->be_user->userGroups as $group){
-				if($group['tx_languagevisibility_allow_movecutdelete_foroverlays']){
+	function allowCutCopyMoveDelete() {
+		$res = false;
+		if (is_array($this->be_user->userGroups)) {
+			foreach ( $this->be_user->userGroups as $group ) {
+				if ($group['tx_languagevisibility_allow_movecutdelete_foroverlays']) {
 
 					$res = true;
 				}
 			}
 		}
-		
+
 		return $res;
 	}
-	
-	
-	public function isAdmin(){
+
+	public function isAdmin() {
 		return $this->be_user->isAdmin();
 	}
-	
+
 	/**
 	 * This method returns the userId of the current backend user.
-	 * 
+	 *
 	 * @return int userId of the backend user
 	 */
-	public function getUid(){
+	public function getUid() {
 		return $this->be_user->user[uid];
 	}
-	
+
 }
 ?>
--- patch/lib/class.tx_languagevisibility_commandMap.php
+++ patch/lib/class.tx_languagevisibility_commandMap.php
@@ -1,56 +1,81 @@
 <?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2009 AOE media <dev@aoemedia.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 /**
  * Wrapperclass for the command map in tce_main
  *
+ * @author	Daniel P�tzinger
+ * @author	Tolleiv Nietsch
  */
-class tx_languagevisibility_commandMap{
+class tx_languagevisibility_commandMap {
 	private $cmd_map;
-	
-	public function setMap($map){
+
+	public function setMap($map) {
 		$this->cmdmap = $map;
 	}
-	
+
 	/**
 	 * Return an array of elements with the command, uid, and table
 	 *
 	 * example:
-	 *  getElementsByCommand('delete');
-	 * 
+	 * getElementsByCommand('delete');
+	 *
 	 * returns all elements in the map it the command delete
 	 * @param array $commands
 	 * @return unknown
 	 */
-	public function getElementsByCommands($commands){
+	public function getElementsByCommands($commands) {
 		$elements = array();
-		
-		foreach($this->cmdmap as $table => $entry){
+
+		foreach ( $this->cmdmap as $table => $entry ) {
 			//traverse records in table with commands
-			foreach($entry as $uid => $cmds){
+			foreach ( $entry as $uid => $cmds ) {
 				//traverse command for record in table
-				foreach($cmds as $cmd => $active){
-					if((bool) $active && in_array($cmd,$commands)){
+				foreach ( $cmds as $cmd => $active ) {
+					if (( bool ) $active && in_array($cmd, $commands)) {
 						//create element instance and append it to the result array
-				
-						$elements[] =	array('cmd' => $cmd, 'uid' => $uid, 'table' => $table);
+
+
+						$elements[] = array('cmd' => $cmd, 'uid' => $uid, 'table' => $table );
 					}
 				}
 			}
 		}
-		
+
 		return $elements;
 	}
-	
+
 	/**
 	 * removes an element from the command map
 	 *
 	 * @param unknown_type $element
 	 */
-	public function removeElement($element){
+	public function removeElement($element) {
 		unset($this->cmdmap[$element['table']][$element['uid']][$element['cmd']]);
 	}
-	
-	public function getMap(){
-		return $this->cmdmap;	
+
+	public function getMap() {
+		return $this->cmdmap;
 	}
 }
 ?>
