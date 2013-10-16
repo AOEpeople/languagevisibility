75674ccd08aff25e9294ae59656ff331bd1880f3
Stefan Rotsch <stefan.rotsch@aoemedia.de>
2013-10-16 11:06:35 +0200

    [TASK] Adjust autoloader configuration
    Change-Id: I1e6bf7268469dc83e815589f3d9632e60ed5d8ad
    Reviewed-on: https://review.typo3.org/24831
    Reviewed-by: Stefan Rotsch
    Tested-by: Stefan Rotsch

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,44 +3,44 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['languagevisibility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_t3lib_tcemain.php:tx_languagevisibility_hooks_t3lib_tcemain';
+/**
+ * Register TYPO3 core hooks
+ */
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['languagevisibility'] = 'tx_languagevisibility_hooks_t3lib_tcemain';
 
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getPageOverlay']['languagevisility'] = 'tx_languagevisibility_hooks_t3lib_page';
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getRecordOverlay']['languagevisility'] = 'tx_languagevisibility_hooks_t3lib_page';
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['checkFullLanguagesAccess']['languagevisility'] = 'tx_languagevisibility_hooks_t3lib_userauthgroup->checkFullLanguagesAccess';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['settingLanguage_preProcess']['languagevisility'] = 'tx_languagevisibility_hooks_tslib_fe->settingLanguage_preProcess';
 
-	// assuming that we get our patch into the TYPO3 core
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getPageOverlay']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_t3lib_page.php:tx_languagevisibility_hooks_t3lib_page';
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getRecordOverlay']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_t3lib_page.php:tx_languagevisibility_hooks_t3lib_page';
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['checkFullLanguagesAccess']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_t3lib_userauthgroup.php:tx_languagevisibility_hooks_t3lib_userauthgroup->checkFullLanguagesAccess';
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['settingLanguage_preProcess']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_tslib_fe.php:tx_languagevisibility_hooks_tslib_fe->settingLanguage_preProcess';
-
-$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/alt_doc.php']['makeEditForm_accessCheck']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_alt_doc.php:tx_languagevisibility_hooks_alt_doc->makeEditForm_accessCheck';
-$TYPO3_CONF_VARS['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages']['languagevisility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_tslib_menu.php:tx_languagevisibility_hooks_tslib_menu';
-
+$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/alt_doc.php']['makeEditForm_accessCheck']['languagevisility'] = 'tx_languagevisibility_hooks_alt_doc->makeEditForm_accessCheck';
+$TYPO3_CONF_VARS['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages']['languagevisility'] = 'tx_languagevisibility_hooks_tslib_menu';
 
 	// overriding option because this is done by languagevisibility and will not work if set
 $TYPO3_CONF_VARS['FE']['hidePagesIfNotTranslatedByDefault'] = 0;
 
-	//adding inheriatance flag to the addRootlineField
-$rootlinefields = &$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'];
-$newRootlinefields = 'tx_languagevisibility_inheritanceflag_original, tx_languagevisibility_inheritanceflag_overlayed';
-$rootlinefields .= (empty($rootlinefields))? $newRootlinefields : ',' . $newRootlinefields;
+	// adding inheriatance flag to the addRootlineField
+$rootlineFields = &$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'];
+$newRootlineFields = 'tx_languagevisibility_inheritanceflag_original, tx_languagevisibility_inheritanceflag_overlayed';
+$rootlineFields .= (empty($rootlineFields))? $newRootlineFields : ',' . $newRootlineFields;
 
 	// adding the inheritance flag to the pageOverlayFields
-$pagesOverlayfields = &$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'];
-$newPagesOverlayfields = 'tx_languagevisibility_inheritanceflag_overlayed';
-$pagesOverlayfields .= (empty($pagesOverlayfields)) ? $newPagesOverlayfields : ',' . $newPagesOverlayfields;
+$pagesOverlayFields = &$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'];
+$newPagesOverlayFields = 'tx_languagevisibility_inheritanceflag_overlayed';
+$pagesOverlayFields .= (empty($pagesOverlayFields)) ? $newPagesOverlayFields : ',' . $newPagesOverlayFields;
 
 /**
- * Extension-Hooks
+ * Register extension hooks
  */
 if (t3lib_extMgm::isLoaded('templavoila')) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['pi1']['renderElementClass']['languagevisibility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_templavoila_pi1.php:tx_languagevisibility_hooks_templavoila_pi1';
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['pi1']['renderElementClass']['languagevisibility'] = 'tx_languagevisibility_hooks_templavoila_pi1';
 }
 if (t3lib_extMgm::isLoaded('crawler')) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['processUrls']['languagevisibility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_crawler.php:tx_languagevisibility_hooks_crawler->processUrls';
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['processUrls']['languagevisibility'] = 'tx_languagevisibility_hooks_crawler->processUrls';
 }
 if (t3lib_extMgm::isLoaded('phpunit')) {
 	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['phpunit']['importExtensions_additionalDatabaseFiles']['languagevisibility'] = 'EXT:languagevisibility/ext_tables.sql';
 }
 if (t3lib_extMgm::isLoaded('aoe_wspreview')) {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['aoe_wspreview/system/class.tx_aoewspreview_system_workspaceService.php']['createDiff']['languagevisibility'] = 'EXT:languagevisibility/hooks/class.tx_languagevisibility_hooks_aoe_wspreview.php:tx_languagevisibility_hooks_aoe_wspreview->aoewspreview_createDiff';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['aoe_wspreview/system/class.tx_aoewspreview_system_workspaceService.php']['createDiff']['languagevisibility'] = 'tx_languagevisibility_hooks_aoe_wspreview->aoewspreview_createDiff';
 }
