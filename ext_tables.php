a96731f4adbcb70d29d72caddf4299f2730b413b
mcambal <martin.cambal@eset.sk>
2018-01-05 10:10:28 +0100

    Removed invalid info module

--- ext_tables.php
+++ ext_tables.php
@@ -7,14 +7,4 @@ $GLOBALS['TCA']['tt_content']['palettes']['general']['showitem'] = str_replace('
 
 $GLOBALS['TCA']['tt_content']['ctrl']['dividers2tabs'] = TRUE;
 
-
-if (TYPO3_MODE=='BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
-		'web_info',
-		'tx_languagevisibility_modfunc1',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'modfunc1/class.tx_languagevisibility_modfunc1.php',
-		'LLL:EXT:languagevisibility/locallang_db.xml:moduleFunction.tx_languagevisibility_modfunc1'
-	);
-}
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['configuration'][] = 'tx_languagevisibility_reports_ConfigurationStatus';
